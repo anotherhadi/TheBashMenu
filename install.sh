872a13cd8f6871939148aa0f2cc56daf2943cad2
Hadrien Aka <hadrienaka@protonmail.com>
2020-10-27 10:29:50 +0100

    Add the install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+rm README.md
+rm menugif.gif
+rm addgif.gif
+chmod +x main.sh
+chmod +x addcommand.sh
+printf "\n\e[0;92m✓ \e[0m\e[1;77mWell done! \e[0;96m [Start in 3s]\e[0m"
+sleep 3
+./main.sh
