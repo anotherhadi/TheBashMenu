65b61602b476c64b93780a193d5c12d503a71b69
Hadrien Aka <hadrienaka@protonmail.com>
2020-10-28 17:15:08 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+rm README.md
+rm exemples.gif
+rm menu.gif
+
 mv bmenu.sh ~/.bmenu
 chmod +x ~/.bmenu
 ########################### ADD TO PATH
@@ -7,5 +11,4 @@ printf "\n\e[0;92m✓ \e[0m\e[1;77mBMenu Is Ready\e[0;96m\e[0m"
 printf "\n\e[0;92m? \e[0m\e[1;77mYou can now create your own menu \e[0;96m\e[0m"
 printf "\n\e[0;92m? \e[0m\e[1;77m\e[0;96m[If you don't know how to do it, check https://github.com/hadrienaka/TheBashMenu]\e[0m"
 printf "\n"
-cd ..
-rm -rf TheBashMenu
+rm install.sh
