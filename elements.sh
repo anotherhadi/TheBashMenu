fea04f879266451da308cc65e564c71576948c8d
Hadrien Aka <hadrienaka@protonmail.com>
2020-10-28 17:57:09 +0100

    Add files via upload

--- elements.sh
+++ elements.sh
@@ -37,10 +37,10 @@
 
   printf "\n\e[0;92m? \e[0m\e[1;77mYes or No ?\e[0;96m [y/n]\e[0m"
   read -n1 yn
-  if [[ $yn == y ]]; then #UP
+  if [[ $yn == y ]]; then
     printf "\n\e[0;92m✓ \e[0m\e[1;77mYes\e[0m"
     echo
-  elif [[ $yn == "n" ]]; then #DOWN
+  elif [[ $yn == "n" ]]; then
     printf "\n\e[0;91mx \e[0m\e[1;77mNo\e[0m"
     echo
   fi
